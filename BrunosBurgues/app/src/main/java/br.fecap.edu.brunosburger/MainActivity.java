8dc71c0a643570b40e2a2b29933f96eaf3be6c13
VictorLopesD <46869529+VictorLopesD@users.noreply.github.com>
2024-03-31 21:10:55 -0300

    teste

--- BrunosBurgues/app/src/main/java/br.fecap.edu.brunosburger/MainActivity.java
+++ BrunosBurgues/app/src/main/java/br.fecap.edu.brunosburger/MainActivity.java
@@ -1,8 +1,8 @@
 package br.edu.fecap.brunosburgues;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
